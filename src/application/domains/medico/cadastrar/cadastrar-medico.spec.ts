f8b461f0b982042d29c25ab8cb03f0dbc651fab4
Rodrigo da Silva <rodrigopds73@gmail.com>
2024-06-16 13:36:57 -0300

    update cadastrar medico service test

--- src/application/domains/medico/cadastrar/cadastrar-medico.spec.ts
+++ src/application/domains/medico/cadastrar/cadastrar-medico.spec.ts
@@ -6,6 +6,10 @@ import { type IMedicoRepository } from '../repository.interface'
 describe('CadastrarMedicoService', () => {
   const medicoRepository = mock<IMedicoRepository>()
 
+  beforeEach(async () => {
+    jest.clearAllMocks()
+  })
+
   test('deve lancar um erro quando o crm ja existir', async () => {
     // arrange
     jest.spyOn(medicoRepository, 'buscarPorCrm').mockResolvedValueOnce([{
@@ -48,5 +52,29 @@ describe('CadastrarMedicoService', () => {
 
     // assert
     expect(result).toEqual(medico)
+  })
+
+  test('deve chamar o repository corretamente', async () => {
+    // arrange
+    jest.spyOn(medicoRepository, 'insert').mockResolvedValueOnce({
+      id: 1,
+      crm: '111',
+      especialidade: 'especialidade',
+      nome: 'nome'
+    })
+    jest.spyOn(medicoRepository, 'buscarPorCrm').mockResolvedValueOnce([])
+    const data = {
+      crm: '111',
+      especialidade: 'especialidade',
+      nome: 'nome'
+    }
+    const sut = new CadastrarMedicoService(medicoRepository)
+
+    // act
+    await sut.execute(data)
+
+    // assert
+    expect(medicoRepository.insert).toHaveBeenCalledTimes(1)
+    expect(medicoRepository.insert).toHaveBeenCalledWith(data)
   })
 })
